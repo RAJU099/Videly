f998b8fbb87c0ada1d8bf8a5c79306a4a6a03a14
RAJU099r <raju.karkend@gmail.com>
2022-07-01 02:19:26 +0530

    MoviesList

---
+++ Videly/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Videly.Models;
+
+namespace Videly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movies() { Name = "!Shark" ,Id=1 };
+            return View(movie);
+        }
+    }
+}
---
+++ Videly/Models/Movies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Videly.Models
+{
+    public class Movies
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+
+    }
+}
