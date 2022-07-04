1ed0a8c3007954c03e58b715a123bb086b59ca7f
Groot <raju.karkend@gmail.com>
2022-07-05 01:17:53 +0530

    lates changes
    Signed-off-by: Groot <raju.karkend@gmail.com>

--- Videly/App_Start/BundleConfig.cs
+++ Videly/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Videly
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
