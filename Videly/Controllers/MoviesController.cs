987ba127f8b190bd05b925e8b7f7dd70582d233c
Groot <raju.karkend@gmail.com>
2022-07-05 02:36:10 +0530

    Latest cahnges

--- Videly/Controllers/MoviesController.cs
+++ Videly/Controllers/MoviesController.cs
@@ -12,8 +12,23 @@ namespace Videly.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var movie = new Movies() { Name = "!Shark" ,Id=1 };
+            var movie = new Movies() { Name = "!Shark" ,Id=1,Dept="CSE",Designation="Developer" };
             return View(movie);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            return Content("id=" + Id);
+        }
+
+        public ActionResult Index(int? pageIndex,string sortyBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 2;
+
+            if (string.IsNullOrWhiteSpace(sortyBy))
+                sortyBy = "relase";
+            return Content(string.Format("pageIndex={0} & sortby={1}", pageIndex, sortyBy));
         }
     }
 }
